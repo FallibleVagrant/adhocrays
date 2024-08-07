9010b1c8ca8024dd1b1ae6bd032db599c0f0e1ab
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-08-06 22:29:15 -0700

    Make it auto-compile.

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
+use std::process::Command;
 use std::path::Path;
 use std::env;
 
+use std::io::Write;
+
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    Command::new("make").args(&["PLATFORM=PLATFORM_DESKTOP", "-C", &format!("{}/raylib/src", dir)]).status().unwrap();
+    Command::new("cp").args(&[&format!("{}/raylib/src/libraylib.a", dir), &format!("{}/lib/", dir)]).status().unwrap();
+    Command::new("cp").args(&[&format!("{}/raylib/src/raylib.h", dir), &format!("{}/lib/", dir)]).status().unwrap();
     println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lib").display());
 }
