5c86f94dd560dbcc6ffa9e25693ad0214352329f
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-08-06 22:47:36 -0700

    Make build.rs create the lib/ directory.

--- build.rs
+++ build.rs
@@ -1,11 +1,16 @@
 use std::process::Command;
 use std::path::Path;
+use std::fs;
+use std::io::ErrorKind;
 use std::env;
 
 use std::io::Write;
 
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    if fs::create_dir(Path::new(&dir).join("lib")).is_err_and(|e| e.kind() != ErrorKind::AlreadyExists) {
+        panic!("Could not create lib/ directory.");
+    }
     Command::new("make").args(&["PLATFORM=PLATFORM_DESKTOP", "-C", &format!("{}/raylib/src", dir)]).status().unwrap();
     Command::new("cp").args(&[&format!("{}/raylib/src/libraylib.a", dir), &format!("{}/lib/", dir)]).status().unwrap();
     Command::new("cp").args(&[&format!("{}/raylib/src/raylib.h", dir), &format!("{}/lib/", dir)]).status().unwrap();
